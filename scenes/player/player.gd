59e00ac388f8ba2643512d72b3034ec8bad71010
honey <crystadarling@yahoo.com>
2026-05-08 22:23:35 -0800

    added camera node to player, adjusted fall physics

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,22 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var GRAVITY_MULTIPLIER = 1.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * GRAVITY_MULTIPLIER
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_released("jump"):
+		velocity.y = velocity.y * 0.5
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
