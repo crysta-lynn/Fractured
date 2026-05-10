f799adbd940ccebae3eaf3923c24b98cb6886b6b
honey <crystadarling@yahoo.com>
2026-05-10 15:02:14 -0800

    reorganized level1 script and renamed to swap_word.gd

--- scenes/levels/swap_map.gd
+++ scenes/levels/swap_map.gd
@@ -4,11 +4,18 @@ extends Node2D
 
 func _ready() -> void:
 	WorldManager.swap.connect(_on_swap)
+	_on_swap(WorldManager.is_mirrored)
 
 func _on_swap(is_mirrored):
 	if is_mirrored:
 		$"original world".visible = false
+		$"original world".process_mode = Node.PROCESS_MODE_DISABLED
+		
 		$"mirrored world".visible = true
+		$"mirrored world".process_mode = Node.PROCESS_MODE_INHERIT
 	else:
 		$"original world".visible = true
+		$"original world".process_mode = Node.PROCESS_MODE_INHERIT
+		
 		$"mirrored world".visible = false
+		$"mirrored world".process_mode = Node.PROCESS_MODE_DISABLED
