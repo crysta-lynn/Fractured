0fef3c933cfb38e108cc894526c5c1fd8ae3adfc
clmcdonald9 <clmcdonald9@alaska.edu>
2026-05-12 16:21:59 -0800

    created a get_room_bounds script the calculates the top, bottom, left, and right of the r ooms area node, and returns them in a dictionary

--- scenes/levels/rooms.gd
+++ scenes/levels/rooms.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
 @onready var game = $"../.."
+@onready var room_shape = $roomArea/CollisionShape2D
+
+func get_room_bounds() -> Dictionary:
+	var size = room_shape.get_shape().size
+	var position = room_shape.global_position
+	
+	var top = position.y - (size.y/2)
+	var bottom = position.y + (size.y/2)
+	var left = position.x - (size.x/2)
+	var right = position.x + (size.x/2)
+	
+	return {"top": top, "bottom": bottom, "left": left,"right": right}
 
 func _ready() -> void:
 	game.swap_req.connect(_on_swap_req)
