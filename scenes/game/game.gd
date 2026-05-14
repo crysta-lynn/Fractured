02fea655034fb3cf0a9493e9d84a31c4f1bbdf91
clmcdonald9 <clmcdonald9@alaska.edu>
2026-05-14 13:16:44 -0800

    limited camera. swaps camera limits between rooms

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var camera = $player/Camera2D
 @onready var player = $player
 
+
 signal swap_req
 
 func _on_swap(is_mirrored):
@@ -13,17 +14,26 @@ func _on_swap(is_mirrored):
 	
 	await fade.fade_out()
 	
-	swap_req.emit()
 	scale.x = -1 if is_mirrored else 1
+	swap_req.emit()
+	
 	set_process_input(true)
 	player.CAN_MOVE = true
 	
 	await fade.fade_in()
 	
 	camera.position_smoothing_enabled = true
-
-func change_room():
 	
 
+func _on_change_camera_limits(room_bounds):
+	camera.limit_left = room_bounds.left
+	camera.limit_right = room_bounds.right
+	camera.limit_bottom = room_bounds.bottom
+	camera.limit_top = room_bounds.top
+
 func _ready() -> void:
+	var rooms = get_tree().get_nodes_in_group("rooms")
 	WorldManager.swap.connect(_on_swap)
+	
+	for room in rooms:
+		room.change_room.connect(_on_change_camera_limits)
--- scenes/levels/rooms.gd
+++ scenes/levels/rooms.gd
@@ -1,25 +1,20 @@
 extends Node2D
 
 @onready var game = $"../.."
-@onready var room_shape = $roomArea/CollisionShape2D
+@onready var room_area = $roomArea
+var room_bounds
 
-signal room_change(bounds)
+signal change_room(bounds)
 
-func get_room_bounds() -> Dictionary:
-	var size = room_shape.get_shape().size
-	var position = room_shape.global_position
-	
-	var top = position.y - (size.y/2)
-	var bottom = position.y + (size.y/2)
-	var left = position.x - (size.x/2)
-	var right = position.x + (size.x/2)
-	
-	return {"top": top, "bottom": bottom, "left": left,"right": right}
+func _on_body_entered(body : Node2D):
+	if body is player:
+		room_bounds = room_area.get_room_bounds()
+		change_room.emit(room_bounds)
 
-func _on_room_area_body_entered(body: Node2D) -> void:
-	room_change.emit(get_room_bounds())
-	
 func _on_swap_req():
+	room_bounds = room_area.get_room_bounds()
+	change_room.emit(room_bounds)
+	
 	if WorldManager.is_mirrored:
 		$"original world".visible = false
 		$"original world".process_mode = Node.PROCESS_MODE_DISABLED
@@ -35,5 +30,8 @@ func _on_swap_req():
 
 
 func _ready() -> void:
+	add_to_group("rooms")
+	
+	room_area.body_entered.connect(_on_body_entered)
 	game.swap_req.connect(_on_swap_req)
 	_on_swap_req()
